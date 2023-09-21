a5e5aba4068fc82e7c1dd6305839d02b34b26c57
Ankit Verma <ankitv9936@gmail.com>
2023-09-21 23:24:45 +0530

    Made delete button

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -1,19 +1,34 @@
 "use client"
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import "./Home.css"
 import Task from './Task'
 
 const Home = () => {
 
-  const [tasks,setTasks]= useState([]);
+  const initialArray = localStorage.getItem("tasks")?JSON.parse(localStorage.getItem("tasks")):[];
+
+  const [tasks,setTasks]= useState([initialArray]);
   const [title,setTitle]= useState("");
   const [description,setDescription]= useState("");
   
   const submitHandler =(e) =>{
     e.preventDefault();
-
     setTasks([...tasks, { title,description }]);
   };
+
+  const deleteTask = (index) =>{
+
+    const filteredArr =tasks.filter((val,i)=>{
+      return i !==index;
+    });
+    setTasks(filteredArr);
+  };
+  
+  useEffect(() => {
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+  }, [tasks] );
+  
+
   return (
     <div className='container'>
       <h1>DAILY GOALS</h1>
@@ -30,7 +45,7 @@ const Home = () => {
       </form>
 
     { tasks.map((item,index) =>(
-       <Task key={index} title={item.title} description={item.description} /> ))
+       <Task key={index} title={item.title} description={item.description} index={index} deleteTask={deleteTask} /> ))
       }
 
     </div>
--- Components/Task.jsx
+++ Components/Task.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import '@/Components/Task.css'
-const Task = ({title,description}) => {
+const Task = ({title,description,deleteTask,index}) => {
   return (
     <div className="task">
         <div >
             <p>{title}</p>
             <span>{description}</span>
         </div>
-        <button>-</button>
+        {/* here deleteTask(index) is passed as parameter in arrow function  
+            if written as onClick={deleteTask(index)}*/}
+        <button onClick={()=>deleteTask(index)}>-</button> 
     </div>
   )
 }
